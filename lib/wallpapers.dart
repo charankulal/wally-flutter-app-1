2b546a9030e09ce60efbf76494e6ea2d0469c405
Charan K <charankulal0241@gmail.com>
2025-03-07 01:06:36 +0530

    Updation to image source

--- lib/wallpapers.dart
+++ lib/wallpapers.dart
@@ -18,66 +18,66 @@ class WallPaper {
 
 List<WallPaper> wallpapers = [
   WallPaper(
-    "Horizon Zero Dawn",
+    "Dog Man",
     WallPaperImage(
-        "https://www.wallpaperflare.com/static/734/272/885/horizon-zero-dawn-playstation-4-logo-aloy-horizon-zero-dawn-wallpaper.jpg",
+        "https://github.com/charankulal/wally-flutter-app-1/blob/master/10954718.jpg",
         ""),
     "",
-    "Guerrilla Games",
+    "Fox Studios",
   ),
   WallPaper(
-    "Metro Exodus",
+    "Last Breath",
     WallPaperImage(
-        "https://assets1.ignimgs.com/2018/12/14/metro-exodus---button-1544750418985.jpg",
+        "https://github.com/charankulal/wally-flutter-app-1/blob/master/14403504.jpg",
         ""),
     "",
-    "4A Games",
+    "Columbia Pictures",
   ),
   WallPaper(
-    "Tom Clancy's The Division 2",
+    "Captain America",
     WallPaperImage(
-        "https://cdn.division.zone/uploads/2019/03/tc-the-division-2-preload-start-times-sizes-header.jpg",
+        "https://github.com/charankulal/wally-flutter-app-1/blob/master/14513804.jpg",
         ""),
     "",
-    "Massive Entertainment",
+    "Marvel Studios",
   ),
   WallPaper(
-    "Resident Evil 2",
+    "The Monkey",
     WallPaperImage(
-        "https://media.playstation.com/is/image/SCEA/resident-evil-2-box-art-01-ps4-us-12dec18?\$native_nt\$",
+        "https://github.com/charankulal/wally-flutter-app-1/blob/master/27714946.jpg",
         ""),
     "",
-    "Capcom",
+    "DC",
   ),
 ];
 
 List<WallPaper> wallpapers2 = [
   WallPaper(
-    "Grand Theft Auto V",
+    "Last Dream",
     WallPaperImage(
-        "https://gpstatic.com/acache/26/25/1/uk/packshot-7136bdab871d6b2c8f07ccc9ad33b4d0.jpg",
+        "https://github.com/charankulal/wally-flutter-app-1/blob/master/4819866.jpg",
         ""),
     "",
-    "Rockstar Games",
+    "Disney Studios",
   ),
   WallPaper(
-    "The Last of Us Part II",
-    WallPaperImage("https://wallpapercg.com/media/ts_orig/18910.webp", ""),
+    "Make it Real",
+    WallPaperImage("https://github.com/charankulal/wally-flutter-app-1/blob/master/4992098.jpg", ""),
     "",
-    "Naughty Dog",
+    "Guitar shop",
   ),
   WallPaper(
-    "Sekiro: Shadows Die Twice",
+    "Paddington in Peru",
     WallPaperImage(
-        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/773966be-6e02-41dd-8ee2-b6c03e90b854/dcef71y-36be97a6-1d92-4e24-97ac-89a5fe7f2266.jpg/v1/fit/w_828,h_466,q_70,strp/sekiro__shadows_die_twice_wallpaper_by_dralucard_dcef71y-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcLzc3Mzk2NmJlLTZlMDItNDFkZC04ZWUyLWI2YzAzZTkwYjg1NFwvZGNlZjcxeS0zNmJlOTdhNi0xZDkyLTRlMjQtOTdhYy04OWE1ZmU3ZjIyNjYuanBnIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.-8oMlvYLDc61N8K0wi9YjoTvLNhUIaXckVeC1z8flTA",
+        "https://github.com/charankulal/wally-flutter-app-1/blob/master/5822536.jpg",
         ""),
     "",
-    "From Software",
+    "Marvel Studios",
   ),
   WallPaper(
-    "Just Cause 4",
+    "The Boy",
     WallPaperImage(
-        "https://s3.gaming-cdn.com/images/products/2666/orig/just-cause-4-cover.jpg",
+        "https://github.com/charankulal/wally-flutter-app-1/blob/master/8852084.jpg",
         ""),
     "",
     "Avalanche Studios",
@@ -86,25 +86,25 @@ List<WallPaper> wallpapers2 = [
 
 List<WallPaper> featuredWallPapers = [
   WallPaper(
-    "Total War: Three Kingdoms",
+    "Mickey 17",
     WallPaperImage(
-        "https://content.totalwar.com/total-war/com.totalwar.www2019/uploads/2019/03/08145252/2.jpg",
+        "https://raw.githubusercontent.com/charankulal/wally-flutter-app-1/refs/heads/master/fw1.jpg",
         ""),
     "",
-    "Feral Interactive",
+    "Feral Studios",
   ),
   WallPaper(
-    "Call of Duty: Modern Warfare",
+    "Black Bag",
     WallPaperImage(
-        "https://images.hdqwalls.com/download/call-of-duty-modern-warfare-2019-4k-cb-1920x1080.jpg",
+        "https://raw.githubusercontent.com/charankulal/wally-flutter-app-1/refs/heads/master/fw1.jpg",
         ""),
     "",
-    "Infinity Ward",
+    "Fox Studios",
   ),
   WallPaper(
-    "Mortal Kombat 11",
+    "Pulse",
     WallPaperImage(
-        "https://static.trueachievements.com/customimages/093902.jpg", ""),
+        "https://raw.githubusercontent.com/charankulal/wally-flutter-app-1/refs/heads/master/fw3.jpg", ""),
     "",
     "NetherRealm Studios",
   )
